01bfd2415b00672efca9b3b9e9d55e18dd9d560d
Mrinal Gupta <mrinalgupta2002@gmail.com>
2026-05-27 00:10:59 +0530

    Updated resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -12,7 +12,7 @@ export const Bio = {
 
   github: "https://github.com/mrinalgupta24",
 
-  resume: "https://drive.google.com/file/d/1yi27gZFAnRZJxbH7dD8U6GgcsTxTbrpA/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1PdIfQzKf5SMzdkgP-YAAAcxDP8Tnmu0R/view?usp=sharing",
 
   linkedin: "https://www.linkedin.com/in/mrinal-gupta-469915236/",
   twitter: "https://twitter.com/MrinalGupta24",
